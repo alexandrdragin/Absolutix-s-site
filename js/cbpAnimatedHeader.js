5088a4b4627c23f3f284807facca3209b6f54a68
Alexandr Dragin <one.eastside@gmail.com>
2016-04-24 19:19:52 +0300

    location.hash
    не смог разобрать, проще заново сделать по моему без бустрапа

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -1,3 +1,14 @@
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+		$('a.page-scroll').bind('click', function(event) {
+				var $anchor = $(this);
+				$('html, body').stop().animate({
+						scrollTop: $($anchor.attr('href')).offset().top
+				}, 1200, 'easeInOutExpo');
+				event.preventDefault();
+		});
+});
+
 /**
  * cbpAnimatedHeader.js v1.0.0
  * http://www.codrops.com
